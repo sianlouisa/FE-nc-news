0140ba99da77e14895d601f03df371d1d8c469cc
SianLouisa <sianlouisa@hotmail.com>
2019-01-09 14:27:06 +0000

    saved user to local storage to prevent log out on page refresh

--- src/App.js
+++ src/App.js
@@ -9,40 +9,61 @@ import Footer from './components/Footer';
 import Login from './components/Login';
 import Search from './components/Search';
 import Date from './components/Date';
-// import Auth from './components/Auth';
+import Auth from './components/Auth';
 import Users from './components/Users';
 import * as api from './api';
 import PostTopic from './components/PostTopic';
+import PostArticle from './components/PostArticle';
 
 class App extends Component {
   state = { user: {} };
+
   render() {
+    const { user } = this.state;
     return (
       <div className="App">
-        {/* <Auth handleSubmit={this.handleSubmit} user={user}> */}
-        <Header />
-        <Login />
-        <Search />
-        <NavBar />
-        <Router className="content">
-          <Content path="/" />
-          <Content path="/topics/:topic/articles" />
-          <Content path="/articles/:id" />
-          <Users path="/users" />
-          <Users path="/users/:id" />
-          <PostTopic path="/post/topic" />
-        </Router>
-        <Sidebar />
-        <Footer />
-        <Date />
-        {/* </Auth> */}
+        <Auth handleSubmit={this.handleSubmit} user={user}>
+          <Header />
+          <Login />
+          <Search />
+          <NavBar />
+          <Router className="content">
+            <Content path="/" />
+            <Content path="/topics/:topic/articles" />
+            <Content path="/articles/:id" />
+            <Users path="/users" />
+            <Users path="/users/:id" />
+            <PostTopic path="/post/topic" />
+            <PostArticle path="/post/article" />
+          </Router>
+          <Sidebar />
+          <Footer />
+          <Date />
+        </Auth>
       </div>
     );
   }
+
+  componentDidMount() {
+    this.getUser();
+  }
+
+  saveUser = () => {
+    const user = this.state.user;
+    localStorage.setItem('user', JSON.stringify(user));
+  };
+
+  getUser = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    this.setState({ user });
+  };
+
   handleSubmit = event => {
     event.preventDefault();
     const user = event.target.username.value;
-    api.getUserById(user).then(user => this.setState({ user }));
+    api.getUserById(user).then(user => {
+      return this.setState({ user }, () => this.saveUser());
+    });
   };
 }
 
