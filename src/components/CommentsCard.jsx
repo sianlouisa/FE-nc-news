93400f07d459aab72aff99b8c629475cc3ecf36a
SianLouisa <sianlouisa@hotmail.com>
2019-01-27 16:45:30 +0000

    repositioned comments view

--- src/components/CommentsCard.jsx
+++ src/components/CommentsCard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Vote from './Vote';
 import Delete from './Delete';
 import Moment from 'react-moment';
+import { Link } from '@reach/router';
 
 const CommentsCard = props => {
   const { comments, article_id, user } = props;
@@ -10,19 +11,22 @@ const CommentsCard = props => {
       <ul className="comments">
         {comments.map(comment => (
           <li id="comment-item" key={comment.comment_id}>
-            <p>{comment.body}</p>
-
-            {comment.author}
-            <br />
+            <div className="vote-body">
+              <Vote
+                comment_id={comment.comment_id}
+                votes={comment.votes}
+                article_id={article_id}
+              />
+              {comment.body}
+              {comment.author === user.username && (
+                <Delete
+                  comment_id={comment.comment_id}
+                  article_id={article_id}
+                />
+              )}
+            </div>
+            <Link to={`/users/${comment.author}`}>{comment.author}</Link>{' '}
             <Moment fromNow>{comment.created_at}</Moment>
-            <Vote
-              comment_id={comment.comment_id}
-              votes={comment.votes}
-              article_id={article_id}
-            />
-            {comment.author === user.username ? (
-              <Delete comment_id={comment.comment_id} article_id={article_id} />
-            ) : null}
           </li>
         ))}
       </ul>
