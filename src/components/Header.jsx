4d123a09a0c00236046c3445e5809b03b1025455
SianLouisa <sianlouisa@hotmail.com>
2019-06-25 19:32:29 +0100

    removed unusused component from code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import '../App.css';
 import { Link } from '@reach/router';
-import Welcome from './Welcome';
+import Button from 'muicss/lib/react/button';
 
-const Header = props => {
+const Header = (props) => {
   const { user, logout } = props;
   return (
-    <>
-      <div className="header">
-        <Link to="/">
-          <img
-            src={process.env.PUBLIC_URL + '/nc-news-logo.png'}
-            alt="logo"
-            className="logo"
-          />
-        </Link>
-      </div>
-      <div className="welcome">
-        <Welcome logout={logout} user={user} />
-      </div>
-    </>
+    <div className="welcome">
+      <p>
+        Hi,
+        {' '}
+        <Link to={`/users/${user.username}`}>{`${user.name}!`}</Link>
+      </p>
+      <Button size="small" className="btn" type="submit" onClick={logout}>
+        Logout
+      </Button>
+    </div>
   );
 };
 
