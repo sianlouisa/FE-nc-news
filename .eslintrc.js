5cc640a798e89daf82af1e6b5b51fb09bd0af93f
SianLouisa <sianlouisa@hotmail.com>
2019-06-28 09:19:16 +0100

    rules for hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,13 @@
 module.exports = {
   extends: 'airbnb',
   parser: 'babel-eslint',
+  plugins: ['react-hooks'],
   rules: {
     'react/jsx-filename-extension': 0,
     'no-shadow': 0,
     'react/sort-comp': 0,
     'no-undef': 1,
+    'react-hooks/rules-of-hooks': 1,
+    'react-hooks/exhaustive-deps': 2,
   },
 };
