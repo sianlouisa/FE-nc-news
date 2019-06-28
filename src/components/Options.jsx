1d3b2ffbbbeb63806f22ba31e5941115b5998269
SianLouisa <sianlouisa@hotmail.com>
2019-06-28 09:20:01 +0100

    refactor to function component and utilized react hooks and useEffect to replace class based functionality

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,92 +1,113 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../App.css';
 import { withStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
-import NativeSelect from '@material-ui/core/NativeSelect';
-import Button from '@material-ui/core/Button';
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  OutlinedInput,
+} from '@material-ui/core';
 import MediaQuery from 'react-responsive';
 
-class Options extends Component {
-  state = {
-    sort_by: 'created_at',
-    sort_ascending: true,
-    p: 1,
+const Options = ({ classes, topics, fetchSortedArticles }) => {
+  const [values, setValues] = useState({
+    sortBy: 'created_at',
+    sortAscending: true,
     limit: 10,
-  };
-  render() {
-    const { classes } = this.props;
-    const { limit, sort_by } = this.state;
-    return (
-      <div className="sort">
-        <MediaQuery minDeviceWidth={450}>
-          <Button
-            variant="outlined"
-            className={classes.button}
-            onClick={() => this.handlePageTurn(-1)}
-            value="-1"
-          >
-            BACK
-          </Button>
-          <form onSubmit={this.handleSubmit} className="sort-options" />
-          <FormControl className={classes.formControl}>
-            <InputLabel shrink htmlFor="age-native-label-placeholder">
-              Limit
-            </InputLabel>
-            <NativeSelect value={limit} onChange={this.handleChange}>
-              <option value={5}>5</option>
-              <option value={10}>10</option>
-              <option value={15}>15</option>
-            </NativeSelect>
-          </FormControl>
-          <FormControl className={classes.formControl}>
-            <InputLabel shrink htmlFor="age-native-label-placeholder">
-              Sort Type
-            </InputLabel>
-            <NativeSelect value={sort_by} onChange={this.handleChange}>
-              <option value="created_at">Newest</option>
-              <option value="votes">Trending</option>
-              <option value="comment_count">Popular</option>
-            </NativeSelect>
-          </FormControl>
-          <Button
-            variant="outlined"
-            className={classes.button}
-            onClick={() => this.handlePageTurn(1)}
-            value="1"
-          >
-            NEXT
-          </Button>
-        </MediaQuery>
-      </div>
-    );
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state !== prevState) {
-      const { topic, fetchSortedArticles } = this.props;
-      const { sort_by, sort_ascending, p, limit } = this.state;
-      fetchSortedArticles({ sort_by, sort_ascending, p, limit }, topic);
-    }
-  }
+  });
+  const [p, setP] = useState(1);
 
-  handleChange = event => {
-    const sort = event.target.value;
-    sort.length <= 2
-      ? this.setState({ limit: sort })
-      : this.setState({ sort_by: sort });
+  const inputLabel = React.useRef(null);
+  const [labelWidth, setLabelWidth] = React.useState(0);
+  React.useEffect(() => {
+    setLabelWidth(inputLabel.current.offsetWidth);
+  }, []);
+
+  useEffect(() => {
+    fetchSortedArticles(
+      {
+        sort_by: values.sortBy,
+        sort_ascending: values.sortAscending,
+        limit: values.limit,
+        p,
+      },
+      topics,
+    );
+  }, [fetchSortedArticles, topics, values, p]);
+
+  const handleChange = (event) => {
+    setValues(oldValues => ({
+      ...oldValues,
+      [event.target.name]: [event.target.value],
+    }));
   };
 
-  handlePageTurn = inc => {
-    const { sort_by, sort_ascending, p, limit } = this.state;
-    const { topic, fetchSortedArticles } = this.props;
-    this.setState(
-      state => ({
-        p: state.p + inc,
-      }),
-      () => fetchSortedArticles({ sort_by, sort_ascending, p, limit }, topic),
-    );
+  const handlePageTurn = (inc) => {
+    setP(p + inc);
   };
-}
+
+  return (
+    <div className="sort">
+      <MediaQuery minDeviceWidth={450}>
+        <Button
+          variant="outlined"
+          className={classes.button}
+          onClick={() => handlePageTurn(-1)}
+          value="-1"
+        >
+          BACK
+        </Button>
+        <form className="sort-options" />
+        <FormControl variant="outlined" className={classes.formControl}>
+          <InputLabel ref={inputLabel} htmlFor="outlined-age-simple">
+            Limit
+          </InputLabel>
+          <Select
+            value={values.limit}
+            onChange={handleChange}
+            input={<OutlinedInput labelWidth={labelWidth} name="limit" id="outlined-age-simple" />}
+          >
+            <MenuItem value="">
+              <em>Limit</em>
+            </MenuItem>
+            <MenuItem value={10}>10</MenuItem>
+            <MenuItem value={15}>15</MenuItem>
+            <MenuItem value={25}>25</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl variant="outlined" className={classes.formControl}>
+          <InputLabel ref={inputLabel} htmlFor="outlined-age-simple">
+            Sort By
+          </InputLabel>
+          <Select
+            value={values.sortBy}
+            onChange={handleChange}
+            input={<OutlinedInput labelWidth={labelWidth} name="sortBy" id="outlined-age-simple" />}
+          >
+            <MenuItem value="">
+              <em>Limit</em>
+            </MenuItem>
+            <MenuItem value="created_at">Newest</MenuItem>
+            <MenuItem value="votes">Trending</MenuItem>
+            <MenuItem value="comment_count">Popular</MenuItem>
+          </Select>
+        </FormControl>
+
+        <Button
+          variant="outlined"
+          className={classes.button}
+          onClick={() => handlePageTurn(1)}
+          value="1"
+        >
+          NEXT
+        </Button>
+      </MediaQuery>
+    </div>
+  );
+};
 
 const styles = theme => ({
   root: {
@@ -98,7 +119,7 @@ const styles = theme => ({
     minWidth: 120,
   },
   selectEmpty: {
-    marginTop: theme.spacing.unit * 2,
+    marginTop: theme.spacing(2),
   },
   button: {
     margin: theme.spacing.unit,
