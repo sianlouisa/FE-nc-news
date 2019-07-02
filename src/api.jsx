9b688e9110cdd890fad6d3caedb9a75a7b314272
SianLouisa <sianlouisa@hotmail.com>
2019-07-02 20:57:41 +0100

    code format refactoring

--- src/api.jsx
+++ src/api.jsx
@@ -9,31 +9,29 @@ export const getTopics = async () => {
 
 // ARTICLES
 
-export const getArticles = async (sort, topic) => {
+export const getArticles = async (sort, topic, page, limit) => {
   if (sort === undefined) {
     if (topic === undefined) {
-      const { data } = await axios.get(`${BASE_URL}/articles`);
-      return data.articles;
-    } else {
-      const { data } = await axios.get(`${BASE_URL}/topics/${topic}/articles`);
+      const { data } = await axios.get(`${BASE_URL}/articles?p=${page}&limit=${limit}`);
       return data.articles;
     }
+    const { data } = await axios.get(
+      `${BASE_URL}/topics/${topic}/articles?p=${page}&limit=${limit}`,
+    );
+    return data.articles;
   }
   if (sort !== undefined) {
-    const { sort_by, sort_ascending, p, limit } = sort;
-    let query = `?limit=${limit}&sort_by=${sort_by}&p=${p}`;
+    const { sort_by, sort_ascending } = sort;
+    let query = `?limit=${limit}&sort_by=${sort_by}&p=${page}`;
     if (sort_ascending === 'true') {
-      query += `&sort_ascending=true`;
+      query += '&sort_ascending=true';
     }
     if (topic === undefined) {
       const { data } = await axios.get(`${BASE_URL}/articles${query}`);
       return data.articles;
-    } else {
-      const { data } = await axios.get(
-        `${BASE_URL}/topics/${topic}/articles${query}`,
-      );
-      return data.articles;
     }
+    const { data } = await axios.get(`${BASE_URL}/topics/${topic}/articles${query}`);
+    return data.articles;
   }
 };
 
@@ -43,9 +41,7 @@ export const articleLimits = async (limit, topic) => {
     return data.articles;
   }
 
-  const { data } = await axios.get(
-    `${BASE_URL}/topics/${topic}/articles?limit=${limit}`,
-  );
+  const { data } = await axios.get(`${BASE_URL}/topics/${topic}/articles?limit=${limit}`);
   return data.articles;
 };
 
@@ -56,9 +52,7 @@ export const articleSortBy = async (sort, topic) => {
     ascStr = '&sort_ascending=true';
   }
   if (topic === undefined) {
-    const { data } = await axios.get(
-      `${BASE_URL}/articles?sort_by=${sort.sort_by}${ascStr}`,
-    );
+    const { data } = await axios.get(`${BASE_URL}/articles?sort_by=${sort.sort_by}${ascStr}`);
     return data.articles;
   }
   const { data } = await axios.get(
@@ -67,26 +61,25 @@ export const articleSortBy = async (sort, topic) => {
   return data.articles;
 };
 
-export const getArticleById = async article_id => {
+export const getArticleById = async (article_id) => {
   const { data } = await axios.get(`${BASE_URL}/articles/${article_id}`);
   return data.articles;
 };
 
 // USERS
 
-export const getUsers = async username => {
+export const getUsers = async (username) => {
   if (username === undefined) {
     const { data } = await axios.get(`${BASE_URL}/users`);
     return data;
-  } else {
-    const { data } = await axios.get(`${BASE_URL}/users/${username}`);
-    return data;
   }
+  const { data } = await axios.get(`${BASE_URL}/users/${username}`);
+  return data;
 };
 
 // COMMENTS
 
-export const getArticleComments = async article_id => {
+export const getArticleComments = async (article_id) => {
   const { data } = await axios.get(
     `${BASE_URL}/articles/${article_id}/comments?sort_by=created_at`,
   );
@@ -96,15 +89,13 @@ export const getArticleComments = async article_id => {
 // QUERIES
 
 export const getArticlesMostComments = async () => {
-  const { data } = await axios.get(
-    `${BASE_URL}/articles?sort_by=comment_count`,
-  );
+  const { data } = await axios.get(`${BASE_URL}/articles?sort_by=comment_count`);
   return data.articles;
 };
 
 // POSTS
 
-export const postTopic = async newTopic => {
+export const postTopic = async (newTopic) => {
   const { data } = await axios.post(`${BASE_URL}/topics`, {
     description: newTopic.description,
     slug: newTopic.slug,
@@ -112,63 +103,51 @@ export const postTopic = async newTopic => {
   return data;
 };
 
-export const postArticle = async newArticle => {
-  const { data } = await axios.post(
-    `${BASE_URL}/topics/${newArticle.topic}/articles`,
-    {
-      title: newArticle.title,
-      body: newArticle.body,
-      topic: newArticle.topic,
-      created_by: newArticle.user_id,
-      article_id: newArticle.article_id,
-    },
-  );
+export const postArticle = async (newArticle) => {
+  const { data } = await axios.post(`${BASE_URL}/topics/${newArticle.topic}/articles`, {
+    title: newArticle.title,
+    body: newArticle.body,
+    topic: newArticle.topic,
+    created_by: newArticle.user_id,
+    article_id: newArticle.article_id,
+  });
   return data;
 };
 
-export const postCommentOnArticle = async newComment => {
-  const { data } = await axios.post(
-    `${BASE_URL}/articles/${newComment.article_id}/comments`,
-    {
-      user_id: newComment.user_id,
-      body: newComment.body,
-      article_id: newComment.article_id,
-    },
-  );
+export const postCommentOnArticle = async (newComment) => {
+  const { data } = await axios.post(`${BASE_URL}/articles/${newComment.article_id}/comments`, {
+    user_id: newComment.user_id,
+    body: newComment.body,
+    article_id: newComment.article_id,
+  });
   return data;
 };
 
 // DELETE
 
-export const deleteItem = async id => {
+export const deleteItem = async (id) => {
   if (id.comment_id === undefined) {
-    const { data } = await axios.delete(
-      `${BASE_URL}/articles/${id.article_id}`,
-    );
-    return data;
-  } else {
-    const { data } = await axios.delete(
-      `${BASE_URL}/articles/${id.article_id}/comments/${id.comment_id}`,
-    );
+    const { data } = await axios.delete(`${BASE_URL}/articles/${id.article_id}`);
     return data;
   }
+  const { data } = await axios.delete(
+    `${BASE_URL}/articles/${id.article_id}/comments/${id.comment_id}`,
+  );
+  return data;
 };
 
 // PATCH
 
-export const vote = async toPatch => {
+export const vote = async (toPatch) => {
   const { article_id, inc, comment_id } = toPatch;
   if (comment_id === undefined) {
-    const { data } = await axios.patch(
-      `${BASE_URL}/articles/${toPatch.article_id}`,
-      { inc_votes: inc },
-    );
-    return data;
-  } else {
-    const { data } = await axios.patch(
-      `${BASE_URL}/articles/${article_id}/comments/${comment_id}`,
-      { inc_votes: inc },
-    );
+    const { data } = await axios.patch(`${BASE_URL}/articles/${toPatch.article_id}`, {
+      inc_votes: inc,
+    });
     return data;
   }
+  const { data } = await axios.patch(`${BASE_URL}/articles/${article_id}/comments/${comment_id}`, {
+    inc_votes: inc,
+  });
+  return data;
 };
